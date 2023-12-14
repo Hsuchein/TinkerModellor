b7a61d41a10785931366ae6b0e5d56db3fe294f6
Hsuchein <Hsuchein0126@outlook.com>
2023-12-14 19:23:23 +0800

    All force field error has been resolved

--- tinkermodellor/tkm.py
+++ tinkermodellor/tkm.py
@@ -3,5 +3,5 @@ from tinkermodellor.model_build._tkm import TinkerModellor as tkm
 if __name__ == '__main__':
     
     new= tkm()
-    new(r'/home/wayne/quanmol/TinkerModelling/tinkermodellor/gromacs.gro',r'/home/wayne/quanmol/TinkerModelling/tinkermodellor/gromacs.top')
-    new.write_tkmsystem(r'/media/wayne/datastorage/data/tinker/ncs/gromacs.xyz')
+    new(r'/media/wayne/datastorage/data/tinker/ncs/steep.gro',r'/media/wayne/datastorage/data/tinker/ncs/gromacs.top')
+    new.write_tkmsystem(r'/media/wayne/datastorage/data/tinker/ncs/steep.xyz')
