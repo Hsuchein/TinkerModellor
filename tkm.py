39987b5748164530264071603db04c5f711f9b07
Hsuchein <Hsuchein0126@outlook.com>
2023-12-17 15:53:22 +0800

    tkm.py para modification

--- tkm.py
+++ tkm.py
@@ -5,46 +5,46 @@ import parmed as pmd
 import time
 
 if __name__ == '__main__':
-    description = 'TinkerModellor contribute to import larger system into tinker'
-    usage = 'python tkm.py -loc loc_file -top top_file -out out_file [options]'
+    description = 'TinkerModellor: A complicated biological system construction tool for Tinker Simulation Programme'
+    usage = 'python tkm.py -c coordination_file -p topology_file -out output_file [options]'
     
     parser = argparse.ArgumentParser(description=description, usage=usage,formatter_class=argparse.RawTextHelpFormatter,)
     
-    parser.add_argument('-loc',
+    parser.add_argument('-c',
                         type = str,
-                        help ='location_file\nthe path to the location_file of the system.\n[Support: Amber(.inpcrd),CHARMM(.crd),GROMACS(.gro)]',
+                        help ='Coordination_file\nthe path to the coordination file of the system.\n[Support: Amber(.inpcrd/.crd),CHARMM(.crd),GROMACS(.gro)]',
                         required = True)
     
-    parser.add_argument('-top',
+    parser.add_argument('-p',
                         type = str,
-                        help = 'topology_file\nthe path to location top file of the system.\n[Support: Amber(.prmtop),CHARMM(.psf),GROMACS(.top)]', 
+                        help = 'Topology_file\nthe path to the topology file of the system.\n[Support: Amber(.prmtop/.top),CHARMM(.psf),GROMACS(.top)]', 
                         required = True)
     
-    parser.add_argument('-out', 
+    parser.add_argument('-o', 
                         type = str,
-                        default= os.path.join(os.getcwd(),time.ctime().split(' ')[-2].replace(':','_')+'.xyz'),
-                        help = 'outfile\ntake current time as default,\n[Default: "./sec_min_hour.xyz"]\n[Format: tinker(.xyz)]', 
+                        default= os.path.join(os.getcwd(),'/TinkerModellor.xyz'),
+                        help = 'Output_file\nthe path or name of output file,\n[Default: "./sec_min_hour.xyz"]\n[Format: tinker(.xyz)]', 
                         )
     
-    parser.add_argument('--keep', 
+    parser.add_argument('-k', 
                         type = bool,
                         default = False,
-                        help = 'keep\nParmed will transfer the input file,\nit creates temporary files then removed,\nyou can choose True to keep it\n[Default: False]', 
+                        help = 'Keep\nParmed will transformate the input file into GROMACS format,\nit would create temporary files then removed,\nyou can set True to keep it\n[Default: False]', 
                         )
     
-    parser.add_argument('--style',
+    parser.add_argument('-f',
                         type = str,
                         choices = ['A','C','G'],
                         default = 'G' ,
-                        help = 'style\nthe style you create the system with ,\n{A: Amber, C: CHARMM, G: GROMACS}\n[default: G]', 
+                        help = 'Format\nthe the format of input file system with ,\n{A: Amber, C: CHARMM, G: GROMACS}\n[default: G]', 
                         )
     
     args = parser.parse_args()
-    top_file = args.topology_file
-    gro_file = args.location_file
-    out_file = args.outfile
-    keep = args.keep
-    program = args.program
+    top_file = args.Topology_file
+    gro_file = args.Coordination_file
+    out_file = args.Output_file
+    keep = args.Keep
+    program = args.Format
     
     if program == 'GROMACS' :
 
